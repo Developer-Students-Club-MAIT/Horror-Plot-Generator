9ce1648b945950ed4122f8bcdbeb7278208fd437
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-10-27 16:26:33 +0530

    final codes added

--- app.py
+++ app.py
@@ -3,15 +3,38 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from utils import *
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from model_class import CharRNN
 
 app = Flask(__name__)
 train_on_gpu = torch.cuda.is_available()
 model = get_model()
 
-@app.route('/api', methods=['GET'])
+@app.route('/')
 def home():
+    return render_template('index.html', choices= [{'name': 'small'}, {'name': 'medium'}, {'name': 'large'}], text='')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    length = 100
+    start_words = 'the'
+    plot_length = 'small'
+    if request.method == 'POST':
+        plot_length = request.form.get('model-select')
+        start_words = request.form.get('ip')
+    if plot_length == 'small':
+        length = 500
+    elif plot_length == 'medium':
+        length = 1200
+    elif plot_length == 'large':
+        length = 2000   
+    start_words = start_words.lower()
+    answer = PlotGenerate(model, length, prime=start_words, top_k=5)
+    return render_template('index.html',choices= [{'name': 'small'}, {'name': 'medium'}, {'name': 'large'}], text=answer)
+
+
+@app.route('/api', methods=['GET'])
+def predict_api():
     length = 100
     start_words = 'the'
     query = request.args
@@ -21,7 +44,7 @@ def home():
     elif plot_length == 'medium':
         length = 1200
     elif plot_length == 'large':
-        length = 2000
+        length = 2000   
     start_words = query.get('start_words')
     answer = PlotGenerate(model, length, prime=start_words, top_k=5)
     return jsonify({'resultant_story': answer})
